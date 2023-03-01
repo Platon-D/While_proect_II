ab1da3d3234ed4f35ed73f7941035e7ab1b402d9
Platon Davydov <purplefunmaster@gmail.com>
2023-03-01 22:22:22 +0200

    While

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        int Ink = 0;
+
+        while (Ink<=20) {
+            System.out.println(Ink);
+            Ink++;
+        }
+        System.out.println("Success. Exit");
+        System.out.println("Start new WOW");
+
+        boolean ink = true;
+        while (ink) {
+            System.out.println("Wow");
+            ink = new Random().nextInt(20) !=8;
+        } System.out.println("ink cycle exit");
+
+
+    }
+}
